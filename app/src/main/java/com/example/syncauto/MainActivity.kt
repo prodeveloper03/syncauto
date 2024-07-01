d70a948b8e9887cb82fdcab204c069d27e52b589
sounak saha <sounaksaha100@gmail.com>
2024-07-02 00:27:51 +0530

    splash screen

--- app/src/main/java/com/example/syncauto/MainActivity.kt
+++ app/src/main/java/com/example/syncauto/MainActivity.kt
@@ -8,8 +8,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.syncauto.ui.splash.SplashScreen
 import com.example.syncauto.ui.theme.SyncautoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +22,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SyncautoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp(){
+    var showSplash by remember { mutableStateOf(true) }
+    if (showSplash){
+        SplashScreen {
+            showSplash = true
+        }
+    }else {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SyncautoTheme {
-        Greeting("Android")
     }
 }
+
---
+++ app/src/main/java/com/example/syncauto/ui/splash/SplashScreen.kt
@@ -0,0 +1,49 @@
+package com.example.syncauto.ui.splash
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.syncauto.R
+import kotlinx.coroutines.delay
+
+
+
+@Composable
+fun SplashScreen(onNavigate: () -> Unit){
+
+    LaunchedEffect(Unit) {
+        delay(5000)
+        onNavigate()
+    }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "App Logo",
+                modifier = Modifier.size(128.dp)
+            )
+        }
+
+    }
+}
+
