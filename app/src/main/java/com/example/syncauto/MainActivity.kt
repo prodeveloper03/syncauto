63173e9f41db1be985ea4a39b528c688a7402184
sounak saha <sounaksaha100@gmail.com>
2024-07-11 00:05:46 +0530

    Date and time feature implemented

--- app/src/main/java/com/example/syncauto/MainActivity.kt
+++ app/src/main/java/com/example/syncauto/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.syncauto
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.widget.EdgeEffect
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -14,10 +17,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavGraph
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.syncauto.ui.navigation.BottomNavBar
-import com.example.syncauto.ui.navigation.NavGraph
 import com.example.syncauto.ui.splash.SplashScreen
 import com.example.syncauto.ui.theme.SyncautoTheme
 
--- app/src/main/java/com/example/syncauto/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/syncauto/ui/home/HomeScreen.kt
@@ -1,21 +1,62 @@
 package com.example.syncauto.ui.home
 
+import android.os.Build
+import androidx.activity.compose.LocalActivityResultRegistryOwner.current
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.unit.dp
+import com.example.syncauto.utils.getCurrentDate
+import com.example.syncauto.utils.getCurrentTime
+import kotlinx.coroutines.delay
+import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.Formatter
+import java.util.Locale
 
 
 @Composable
+@Preview
 fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Home Screen")
+
+    // Global State
+
+    var currentTime by remember { mutableStateOf(getCurrentTime()) }
+    var currentDate by remember { mutableStateOf(getCurrentDate()) }
+
+
+    LaunchedEffect(Unit) {
+
+        while (true){
+            currentTime = getCurrentTime()
+            delay(1000L)
+        }
+    }
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
+
+    ){
+        Text(text = "$currentTime / $currentDate" , style = MaterialTheme.typography.bodyMedium)
     }
 }
+
+
